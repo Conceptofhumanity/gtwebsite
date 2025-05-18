12c338169fd85d98f45844be2f8fd30a837949a5
Conceptofhumanity <anastasiad.coolgirl@gmail.com>
2025-05-17 20:02:01 -0400

    Update google_translator_updated.py

--- google_translator_updated.py
+++ google_translator_updated.py
@@ -5,42 +5,35 @@ import time
 import random
 
 wait_time = 5
-iterations = 50
-text = """
-hello this is a lot of text a large amount of text to translate so it'll actually have interesting results
-"""
+
 app = Flask(__name__)
 
-@app.route('/translate_text', methods=['POST'])  
-
-def translate_text():  
-    #nabs the text from the website, yk request.get dunno what json is about
-    text = request.json.get('text')  
-    result = translate(text['text'], text['iterations'])  
-    return jsonify({"translated_text": result})  
+@app.route('/translate', methods=['POST'])  
 
 def translate():
+    print("Incoming JSON:", request.get_json())
+    data = request.get_json()
+    current_text = data.get('text', '')
+    iterations = int(data.get('iterations', 100))
+
     driver = webdriver.Chrome()
     languages = [
         'ja', 'ko', 'ru', 'ar', 'hi', 'es', 'fr', 'en', 'bg', 'cs',
         'pl', 'sk', 'zh', 'gu', 'zh-TW', 'af', 'ar', 'da', 'de', 'id', 'it',
         'fa', 'nl'
     ]
-    current_text = text
+
 
     try:
         available_languages = languages.copy()
         for i in range(iterations):
-            #if each language has been used, reset the language pool
             if not available_languages:
                 available_languages = languages.copy()
             lang = random.choice(available_languages)
             available_languages.remove(lang)
 
-            #go to google translate
             driver.get(f"https://translate.google.com/?sl=auto&tl={lang}")
 
-            #find input box, type text.
             input_box = driver.find_element(By.CLASS_NAME, "er8xn")
             input_box.clear()
             input_box.send_keys(current_text)
@@ -65,7 +58,9 @@ def translate():
         results = driver.find_elements(By.CLASS_NAME, "ryNqvb")
         final_text = "\n".join([result.text for result in results])
 
-        print(f"Final Translation: \n {current_text}")
+        print(f"Final Translation: \n {final_text}")
+        print("Data received:", request.get_json())
+        return jsonify({"translated_text": final_text}) 
 
     except Exception as e:
         print(f"Error in translation (i+1): {e}")
