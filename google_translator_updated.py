774e314433a9e3841dc03abd4c8334718187045b
Conceptofhumanity <anastasiad.coolgirl@gmail.com>
2025-05-19 17:38:09 -0400

    Update google_translator_updated.py

--- google_translator_updated.py
+++ google_translator_updated.py
@@ -3,17 +3,19 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 import random
+from flask_cors import CORS
 
-wait_time = 5
 
 app = Flask(__name__)
-
+CORS(app) 
+wait_time = 5
 @app.route('/translate', methods=['POST'])  
 
 def translate():
     print("Incoming JSON:", request.get_json())
     data = request.get_json()
     current_text = data.get('text', '')
+    print(current_text)
     iterations = int(data.get('iterations', 100))
 
     driver = webdriver.Chrome()
@@ -26,6 +28,8 @@ def translate():
 
     try:
         available_languages = languages.copy()
+        if iterations == "":
+            iterations = 10
         for i in range(iterations):
             if not available_languages:
                 available_languages = languages.copy()
@@ -59,7 +63,6 @@ def translate():
         final_text = "\n".join([result.text for result in results])
 
         print(f"Final Translation: \n {final_text}")
-        print("Data received:", request.get_json())
         return jsonify({"translated_text": final_text}) 
 
     except Exception as e:
@@ -71,4 +74,7 @@ def translate():
         driver.quit
 
 if __name__ == '__main__':
+    app.run(debug=True)
+
+
     app.run(debug=True)
